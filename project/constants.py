0ea5da5b2703f19bce06f59325de332d10a4af77
whoIsClownHere <106624742+whoIsClownHere@users.noreply.github.com>
2023-01-25 00:46:09 +0300

    Add files via upload

---
+++ project/constants.py
@@ -0,0 +1,16 @@
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 700
+SCREEN_TITLE = 'Plane'
+FPS = 60
+
+v = 700
+v_attack = 2500
+v_second_attack = 1500
+v_boss_attack = 1200
+v_background = 1
+v_boss = 100
+v_meteor = 600
+
+
+BOSS_HEARTS = 600
+PLAYER_HEARTS = 5
